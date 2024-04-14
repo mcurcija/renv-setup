335aa0f65c2e5cf2870ce37e4eb45cfe44300c36
Mario Curcija <mario.curcija@gmail.com>
2024-04-14 09:34:58 +0200

    first commit

---
+++ renv-setup.R
@@ -0,0 +1,11 @@
+# https://rstudio.github.io/renv/articles/renv.html#collaboration
+# run renv::restore() in order to restore environment
+
+library(tidyverse)
+library(readxl)
+library(ggdist)
+
+# print status
+renv::status()
+
+demo(colors)
